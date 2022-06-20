590d7fbc3dbfccf22a58fc8b5e2f62805d1445d0
dino3616 <suraimu3616@outlook.jp>
2022-06-21 00:13:18 +0900

    style: 💄 ヘッダーのpaddingを32pxから24pxに修正

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import LinkList from './components/LinkList';
 
 const Header = () => (
-  <div className="p-8 shadow-md">
+  <div className="p-6 shadow-md">
     <div className="flex relative justify-center items-center space-x-16">
       <LinkList />
     </div>
