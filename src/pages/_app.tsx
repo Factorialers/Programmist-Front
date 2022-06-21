b8715cb5c0da99204e11bc1c60a9646954e0dbdf
dino3616 <suraimu3616@outlook.jp>
2022-06-22 02:00:09 +0900

    refactor: ♻️ 不要なimportを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss';
-import React from 'react'
 
-const CustomApp = ({ Component, pageProps }: AppProps) => (
-	<Component {...pageProps} />
-)
+const CustomApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
 export default CustomApp;
