c1aa7b874d93c46a95c855bf98efed4a1f566460
dino3616 <suraimu3616@outlook.jp>
2022-06-22 02:00:26 +0900

    style: 💄 Daisy UIのthemeを適用

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+const CustomDocument = () => (
+  <Html lang="ja" data-theme="programmist">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default CustomDocument;
