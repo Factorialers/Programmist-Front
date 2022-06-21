5f0ab0ef2a10a9a44ef9cc28b4c05a148f0bbd48
dino3616 <suraimu3616@outlook.jp>
2022-06-22 02:24:50 +0900

    refactor: ♻️ パスを修正

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import type { FC, ReactNode } from 'react';
-import Footer from '../Footer/Footer';
+import Footer from '../Footer';
 import Header from '../Header';
 
 type Props = {
