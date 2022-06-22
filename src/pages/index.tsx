2c6c09c7ac9fa345a235e25673ed93bf33e58fa4
dino3616 <suraimu3616@outlook.jp>
2022-06-22 21:41:15 +0900

    style: 💄 ダミーコンテンツのy方向のマージンを設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 
 const Home: NextPage = () => (
   <Layout pageTitle="Programmist">
-    <div className="m-5 font-serif text-5xl text-center underline">Hello, Programmist World!</div>
+    <div className="my-80 font-serif text-5xl text-center underline">Hello, Programmist World!</div>
   </Layout>
 );
 
