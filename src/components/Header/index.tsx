cacb3da8e10f449ecddf8e364f12e1a3591b0411
dino3616 <suraimu3616@outlook.jp>
2022-06-22 14:30:06 +0900

    style: 💄 ヘッダーのbackgroundColorを指定

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import LinkList from './components/LinkList';
 import Modal from './components/Modal';
 
 const Header = () => (
-  <div className="grid grid-cols-10 p-6 shadow-md">
+  <div className="grid grid-cols-10 p-6 bg-base shadow-md">
     <div className="flex col-span-8 col-start-2 justify-center items-center space-x-16">
       <LinkList />
     </div>
