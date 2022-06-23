8d0a9e710a7b51f8e81fa3000cbf7b4ff83a745d
dino3616 <suraimu3616@outlook.jp>
2022-06-24 02:16:31 +0900

    feat: 💡 サインインとサインアップでマウントするコンポーネントが変わるように修正

--- src/components/Header/components/PopWindow.tsx
+++ src/components/Header/components/PopWindow.tsx
@@ -1,52 +1,46 @@
 /* eslint-disable tailwindcss/no-custom-classname */
-import Router from 'next/router';
-import { FcGoogle } from 'react-icons/fc';
-import { VscGithub } from 'react-icons/vsc';
-import { loginWithGoogle, loginWithGitHub } from '../../../libs/firebase/auth';
+import { FC, useState } from 'react';
+import Toggle from '../../Toggle';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 
-const PopWindow = () => (
-  <div
-    id="pop_window"
-    role="button"
-    className="flex absolute inset-0 m-auto w-80 h-52 text-center bg-base rounded-lg shadow-xl drop-shadow animate-appear "
-    onClick={(event) => {
-      event.stopPropagation();
-    }}
-    onKeyDown={(event) => {
-      event.stopPropagation();
-    }}
-    tabIndex={0}
-  >
-    <div>
-      <p className="m-5 font-bold">Login to Programmist</p>
-      <button
-        type="button"
-        className="mx-5 mb-5 font-semibold text-neutral hover:text-primary normal-case bg-primary hover:bg-neutral border-none shadow-lg daisy-btn daisy-btn-wide"
-        onClick={async () => {
-          await loginWithGoogle();
-          Router.push('/');
-        }}
-      >
-        <div className="mr-3">
-          <FcGoogle />
+const PopWindow: FC = () => {
+  const [signMode, setSignMode] = useState<'sign-in' | 'sign-up'>('sign-in');
+
+  return (
+    <div
+      id="pop_window"
+      role="button"
+      className="flex absolute inset-0 m-auto w-80 h-64 text-center bg-base rounded-lg shadow-xl drop-shadow animate-appear "
+      onClick={(event) => {
+        event.stopPropagation();
+      }}
+      onKeyDown={(event) => {
+        event.stopPropagation();
+      }}
+      tabIndex={0}
+    >
+      <div>
+        <p className="mt-5 font-bold">Join Programmist</p>
+        <div className="m-5">
+          <div className="flex justify-center">
+            <div className="my-auto mx-3 font-semibold">Sign In</div>
+            <Toggle
+              onClick={() => {
+                if (signMode === 'sign-in') {
+                  setSignMode('sign-up');
+                } else {
+                  setSignMode('sign-in');
+                }
+              }}
+            />
+            <div className="my-auto mx-3 font-semibold">Sign Up</div>
+          </div>
         </div>
-        Login with Google
-      </button>
-      <button
-        type="button"
-        className="mx-5 mb-5 font-semibold text-neutral hover:text-primary normal-case bg-primary hover:bg-neutral border-none shadow-lg daisy-btn daisy-btn-wide"
-        onClick={async () => {
-          await loginWithGitHub();
-          Router.push('/');
-        }}
-      >
-        <div className="mr-3">
-          <VscGithub />
-        </div>
-        Login with GitHub
-      </button>
+        {signMode === 'sign-in' ? <SignIn /> : <SignUp />}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PopWindow;
