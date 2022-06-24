d607f176ee359cb0ef01e24da65e08e857e13aec
dino3616 <suraimu3616@outlook.jp>
2022-06-25 07:04:27 +0900

    feat: 💡 GraphQL Endpoint初期化時にGRAPHQL_ENDPOINT_DEVELOP環境変数も参照するように修正

--- src/libs/urql/index.ts
+++ src/libs/urql/index.ts
@@ -3,9 +3,9 @@ import { authExchange } from '@urql/exchange-auth';
 import { createClient, ClientOptions, cacheExchange, dedupExchange, fetchExchange } from 'urql';
 import authConfig from './authConfig';
 
-const url = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
+const url = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_DEVELOP || process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
 if (!url) {
-  throw new Error('NEXT_PUBLIC_GRAPHQL_ENDPOINTは必須の環境変数です.');
+  throw new Error('"NEXT_PUBLIC_GRAPHQL_ENDPOINT"または"NEXT_PUBLIC_GRAPHQL_ENDPOINT_DEVELOP"は必須の環境変数です.');
 }
 
 const clientOptions: ClientOptions = {
