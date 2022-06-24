f017c1747e9da0e2fa5ae30eb072a29af355c8af
dino3616 <suraimu3616@outlook.jp>
2022-06-25 06:24:46 +0900

    feat: 💡 <Avatar>のpropsにsizeを追加

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -3,12 +3,12 @@ import type { User } from 'firebase/auth';
 import Image from 'next/image';
 import type { FC } from 'react';
 
-const Avatar: FC<{ user: User }> = ({ user }) => (
+const Avatar: FC<{ size: number; user: User }> = ({ size, user }) => (
   <div>
     <div className="daisy-avatar daisy-placeholder">
-      <div className="w-[50px] bg-neutral-focus text-neutral-content daisy-mask daisy-mask-squircle">
+      <div className="bg-neutral-focus text-neutral-content daisy-mask daisy-mask-squircle" style={{ width: size }}>
         {user.photoURL ? (
-          <Image loader={() => `${user.photoURL}?w=50`} src={user.photoURL} width={50} height={50} alt="avatar" />
+          <Image loader={() => `${user.photoURL}?w=${size}`} src={user.photoURL} width={size} height={size} alt="avatar" />
         ) : (
           <span className="text-3xl">{user.uid.slice(0, 2)}</span>
         )}
