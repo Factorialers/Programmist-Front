a9223dfa200d47d826381bccd4a57f9f6c44b2d2
dino3616 <suraimu3616@outlook.jp>
2022-07-12 09:19:10 +0900

    feat: 💡 Editorにタイトルとタグを入力する欄を作成

--- src/features/edit/components/Editor/index.tsx
+++ src/features/edit/components/Editor/index.tsx
@@ -1,20 +1,35 @@
 import type { FC } from 'react';
+import { useRecoilState } from 'recoil';
+import workState from '../../../../libs/recoil/edit/workState';
 import MdEditor from './components/MdEditor';
 import TagsInput from './components/TagsInput';
 
-const Editor: FC = () => (
-  <div>
-    <div className="m-5 flex justify-center">
-      <input
-        placeholder="Title"
-        className="h-14 w-1/2 border-none bg-transparent text-center text-4xl font-semibold text-primary-content outline-none"
+const Editor: FC = () => {
+  const [work, setWork] = useRecoilState(workState);
+
+  return (
+    <div>
+      <div className="m-5 flex justify-center">
+        <input
+          placeholder="Title"
+          value={work.title || ''}
+          className="h-14 w-1/2 border-none bg-transparent text-center text-4xl font-semibold text-primary-content outline-none"
+          onChange={(event) => {
+            setWork({ ...work, title: event.target.value });
+          }}
+        />
+      </div>
+      <div className="m-5 flex justify-center">
+        <TagsInput placeholder="tags" />
+      </div>
+      <MdEditor
+        markdownRecoil={work.markdown}
+        setMarkdownRecoil={(markdown) => {
+          setWork({ ...work, markdown });
+        }}
       />
     </div>
-    <div className="m-5 flex justify-center">
-      <TagsInput placeholder="tags" />
-    </div>
-    <MdEditor />
-  </div>
-);
+  );
+};
 
 export default Editor;
